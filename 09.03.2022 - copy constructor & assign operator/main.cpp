ad3603c3141f3e70a126bc63c74e7a467ade70c1
Teodora Yovcheva <t.k.yovcheva@gmail.com>
2022-03-09 16:21:42 +0200

    copy constructor & assignment operator

--- 09.03.2022 - copy constructor & assign operator/main.cpp
+++ 09.03.2022 - copy constructor & assign operator/main.cpp
@@ -22,11 +22,25 @@ public:
         for (int i = 0; i < this->size; i++) {
             this->array[i] = test.array[i];
         }
-        
         cout << "In copy constructor" << endl;
     }
 
-    
+    Test& operator=(const Test& test) {
+        cout << "In assignment operator" << endl;
+
+        if(this == &test) {
+            cout << "No self-assignment allowed!" << endl;
+        } else {
+            this->size = test.size;
+            delete [] this->array;
+            this->array = new int[this->size];
+            for (int i = 0; i < this->size; i++) {
+                this->array[i] = test.array[i];
+            }
+        }
+        return *this;
+    }
+
     ~Test() {
         cout << "In desctructor" << endl;
     }
@@ -39,31 +53,38 @@ public:
 int main() {
     // Test t1; & Test* t1 = new Test(); 
     // will both call the default constructor
+    
     Test* t = new Test();
+    //Test t1;
     cout << "t memory block: " << &t << endl;
-   
+
     Test test(1);
     cout << "test memory block: "<< &test << endl;
     
-    Test test2(2);
+    Test test2(test);
     cout << "test2 memory block: "<< &test2 << endl;
     
     // Copy constructor
     Test test3 = test; 
     cout << "test3 memory block: "<< &test3 << endl;
 
-
     //Override = operator
     test = test2;
     
+    cout << test.getSize() << endl;
+    cout << test2.getSize() << endl; 
+    // int a =5;
+    // int b=4;
+    // a = b; //a=4;
     cout << "test memory block: "<< &test << endl;
     cout << "test2 memory block: "<< &test2 << endl;
     cout << "test3 memory block: "<< &test3 << endl;
 
-    cout << test.getSize() << endl;
-    cout << test2.getSize() << endl; 
-
+    test = test2 = test3;
+    
     //Calls the destructor of every object
     //at the end of the programme
     return 0;
  }
+
+ // tihomir, nadq, momchil
