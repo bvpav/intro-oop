50cefd5108c463f75b16560b4a6c3047886168cc
Yovcheva <teodora.yovcheva@vistajet.com>
2022-04-27 11:45:33 +0300

    Exercise on sstream and fstream

---
+++ 27-04-2022 - exercise in class on sstream & fstream/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<sstream>
+#include<fstream>
+using namespace std;
+
+// Task 1: Read from console until the user 
+// enters 'q' or 'Q'. Write the text from the console
+// to a txt file. Every time you run your programme,
+// the text should be overwritten.
+// Use stringstream and fstream
+
+// Task 2: Read the .txt file and count the words in it,
+// as well as the numbers.
+// Example output for task 2:
+// Words: 10
+// Numbers: 2
+// Use stringstream and fstream
+
+int main() {
+    return 0;
+}
